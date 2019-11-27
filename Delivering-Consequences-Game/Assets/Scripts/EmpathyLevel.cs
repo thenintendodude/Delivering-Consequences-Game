8de23380bd0e50643a481f0e4e0af9639e9347f8
Alannah Woodward <alannah.woodward98@gmail.com>
2019-11-26 20:24:19 -0800

    UI bars made

---
+++ Delivering-Consequences-Game/Assets/Scripts/EmpathyLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EmpathyLevel : MonoBehaviour
+{
+    public Slider empathyBar;
+    private int maxEmpathy = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        empathyBar.value = 0;
+        empathyBar.maxValue = maxEmpathy;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            empathyBar.value++;
+        } else if(Input.GetKeyDown(KeyCode.G))
+        {
+            empathyBar.value--;
+        }
+    }
+}
