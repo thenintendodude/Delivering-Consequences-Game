4a6182257e24f6daa2c67920ff291bd47161a0f4
Emily Brickey <esbrickey@ucdavis.edu>
2019-12-02 15:04:38 -0800

    Added Ability to Enter and Exit Homes

---
+++ Delivering-Consequences-Game/Assets/Scripts/RedDoorEntry.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedDoorEntry : MonoBehaviour
+{
+    [SerializeField] private GameObject MainCamera;
+    [SerializeField] private GameObject RoomCamera;
+
+    [SerializeField] private Vector3 RoomSpawn;
+    [SerializeField] private GameObject Player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggered Door");
+        //Player enters door of red house
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Switching Cameras");
+            this.MainCamera.SetActive(false);
+            this.RoomCamera.SetActive(true);
+            Player.transform.position = RoomSpawn;
+        }
+    }
+}
---
+++ Delivering-Consequences-Game/Assets/Scripts/RedDoorExit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedDoorExit : MonoBehaviour
+{
+    [SerializeField] private GameObject MainCamera;
+    [SerializeField] private GameObject RoomCamera;
+
+    [SerializeField] private Vector3 OutdoorSpawn;
+    [SerializeField] private GameObject Player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggered Doormat");
+        //Player enters door of red house
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Switching Cameras To Outdoor");
+            this.RoomCamera.SetActive(false);
+            this.MainCamera.SetActive(true);
+            Player.transform.position = this.OutdoorSpawn;
+        }
+    }
+}
